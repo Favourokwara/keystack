8b12f1977a3cb7cfdefb3d71488968f298e8699e
Favour Okwara <favourprogrammer@gmail.com>
2024-01-12 08:45:55 +0000

    feat: :art: Restructured Word  component to use letters

---
+++ frontend/src/app/(home)/_components/Word.tsx
@@ -0,0 +1,37 @@
+import { forwardRef } from "react";
+import Letter from "./Letter";
+
+interface LetterProps {
+	reference: string;
+	expected: string;
+}
+
+const Word = forwardRef<HTMLDivElement, LetterProps>(
+	({ reference, expected }, focusRef) => {
+		const cls = ["word"];
+
+		const isFocused = Boolean(focusRef);
+		const isOmitted = expected.length >= reference.length;
+
+		if (isFocused) cls.push("active"); // mark as active if word is focus
+
+		if (!isFocused && expected.length > 0 && expected !== reference) {
+			cls.push("error");
+		}
+
+		return (
+			<div className={cls.join(" ")} ref={isOmitted ? focusRef : null}>
+				{(isOmitted ? expected : reference).split("").map((_, index) => (
+					<Letter
+						key={index}
+						expected={expected[index]}
+						reference={reference[index]}
+						ref={!isOmitted && index === expected.length ? focusRef : null}
+					/>
+				))}
+			</div>
+		);
+	}
+);
+
+export default Word;
---
+++ frontend/src/app/(home)/_components/Words.tsx
@@ -0,0 +1,32 @@
+import React, { forwardRef } from "react";
+import Word from "./Word";
+
+interface Props {
+	expected: string[];
+	isFocused: boolean;
+	reference: string[];
+	transform?: number;
+}
+
+const Words = forwardRef<HTMLDivElement, Props>(
+	({ expected, isFocused, reference, transform }, focusRef) => {
+		return (
+			<div
+				id="words"
+				className={isFocused ? "" : "blurred"}
+				style={{ transform: `translateY(-${transform || 0}px)` }}
+			>
+				{reference.map((word: string, index: number) => (
+					<Word
+						key={index}
+						reference={word}
+						expected={expected[index]}
+						ref={index === expected.length - 1 ? focusRef : null}
+					/>
+				))}
+			</div>
+		);
+	}
+);
+
+export default Words;
